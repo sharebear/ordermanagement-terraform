c7b1824793df268c0057fecee6f15124d707b44b
Jonathan Share <jonny@sharebear.co.uk>
2017-06-04 23:57:53 +0200

    feat: IAM policy to give read/write access to orders table
    This commit adds an IAM policy that only gives read-write access to the orders dynamodb table and nothing else.
    
    This policy is then attached to the sample-developer user so that a local developer can run the API locally against the AWS dynamodb instance.

--- ordermanagment.tf
+++ ordermanagment.tf
@@ -10,6 +10,36 @@ resource "aws_iam_access_key" "sample-developer" {
   user = "${aws_iam_user.sample-developer.name}"
 }
 
+data "aws_iam_policy_document" "ordermanagement-api-runtime" {
+  statement {
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+      "dynamodb:UpdateItem",
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.orders.arn}",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ordermanagement-api-runtime" {
+  name   = "ordermanagement-api-runtime"
+  policy = "${data.aws_iam_policy_document.ordermanagement-api-runtime.json}"
+}
+
+resource "aws_iam_policy_attachment" "ordermanagement-api-runtime" {
+  name       = "ordermanagement-api-runtime"
+  users      = ["${aws_iam_user.sample-developer.name}"]
+  policy_arn = "${aws_iam_policy.ordermanagement-api-runtime.arn}"
+}
+
 resource "aws_dynamodb_table" "orders" {
   name           = "orders"
   read_capacity  = 5
