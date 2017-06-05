895a123a1502fa6157702dd65b758f2a291902f1
Jonathan Share <jonny@sharebear.co.uk>
2017-06-06 00:42:54 +0200

    fix: version should not be part of the primary key

--- ordermanagment.tf
+++ ordermanagment.tf
@@ -7,16 +7,10 @@ resource "aws_dynamodb_table" "orders" {
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "orderId"
-  range_key      = "version"
 
   attribute {
     name = "orderId"
     type = "S"
-  }
-
-  attribute {
-    name = "version"
-    type = "N"
   }
 
   tags {
